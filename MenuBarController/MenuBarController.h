3fa7b8bb27fa5979b35187d3193cd96c49115243
maios <mmai.iossg@gmail.com>
2018-05-01 11:13:39 +0300

    Initial Commit

---
+++ MenuBarController/MenuBarController.h
@@ -0,0 +1,19 @@
+//
+//  MenuBarController.h
+//  MenuBarController
+//
+//  Created by Mai Mai on 5/1/18.
+//  Copyright © 2018 maimaios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MenuBarController.
+FOUNDATION_EXPORT double MenuBarControllerVersionNumber;
+
+//! Project version string for MenuBarController.
+FOUNDATION_EXPORT const unsigned char MenuBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MenuBarController/PublicHeader.h>
+
+
